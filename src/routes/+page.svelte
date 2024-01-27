182811f47e4ae661101608bf49d1a43c5d55646c
mikeyesteban <michaelp.esteban@gmail.com>
2024-01-27 08:29:18 -0500

    added reset styles, bulma.io, and edited home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="column is-full">
+    <h1 class="title has-text-primary">Welcome to Pokemon Battle Simulator</h1>
+    <p class="subtitle">Special thanks to <a class="has-text-link" href="https://pokeapi.co/">pokeapi.co</a> to allow using the data!</p>
+    <button class="button">Enter</button>
+</div>
+
+<style>
+    .is-full {
+        height: 80vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
